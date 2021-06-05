07309f671578714b8092741990c910abc359af48
tgriff21 <74691411+tgriff21@users.noreply.github.com>
2021-06-05 17:27:59 -0400

    Delete docs directory

--- docs/highscores.js
+++
@@ -1,7 +0,0 @@
-const highscoreList = document.querySelector('#highscoreList');
-const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
-
-highscoreList.innerHTML = 
-highScores.map(score => {
-    return `<li class="high-score"> ${score.name} - ${score.score} </li>`
-}).join('')
